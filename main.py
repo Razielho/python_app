28ed797600c0239b0af759d973993d3f16ed2802
raziel.horovitz <raziel.horovitz@matrix.co.il>
2021-08-27 10:27:02 +0300

    establishe connection to default database

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from os import error
 import mongoengine
 import sys
+from mongoengine.connection import connect, disconnect
 import yaml
 import json
 import logging
@@ -9,20 +11,28 @@ FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 logging.basicConfig(handlers=[logging.FileHandler('mongoengine.log', 'w', 'utf-8')],level=logging.INFO,format=FORMAT)
 log=logging.getLogger()
 
+def connect2mongo():
+    try:
+        mydb=connect(host="mongodb://admin:admin@localhost:27027/test?authSource=admin")
+        log.info("connection established to default datatbase")
+    except:
+        log.error("connection error",error)
+        exit(8)
+    return mydb
+
 ### main program ###
 def main():
 
     log.info("program started")
+    mydb=connect2mongo()
+
 
     # read yaml configuration file
     # params=read_configuration_file()
     # print(params)
     
-    # list projects 4 ingests
-    #projectList=get_projects_list(params['sourceDir'])
-    #print(projectList)
-    #download_biosamples(projectList,params['sourceDir'])
-    
+    disconnect(mydb)
+    log.info("closing connection to default database")
     log.info("program ended")
     
 if __name__ == "__main__":
