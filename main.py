efbcf1eaef363343a7d21c615460d76105e4197e
raziel.horovitz <raziel.horovitz@matrix.co.il>
2021-08-28 18:17:45 +0300

    reference and cascade fields

--- main.py
+++ main.py
@@ -32,23 +32,63 @@ class Page(DynamicDocument):
     comments = ListField(EmbeddedDocumentField(Comment))
     meta = {'collection': 'cmsPage'}
 
+class User(Document):
+    name = StringField()
+
+class Page2(Document):
+    content = StringField()
+    author = ReferenceField(User)
+    boss = ReferenceField('self')
+
+#class ProfilePage(Document):
+#    content = StringField()
+
+class Employee(Document):
+    name = StringField()
+    boss = ReferenceField('self')
+#    profile_page = ReferenceField('ProfilePage')
+
+class ProfilePage(Document):
+    employee = ReferenceField(Employee, reverse_delete_rule=mongoengine.CASCADE)
+    content = StringField()
+
+
 ### main program ###
 def main():
 
     log.info("program started")
     mydb=connect2mongo()
 
+    """
     comment1 = Comment(content='Good work!')
     comment2 = Comment(content='Nice article!')
 
     page = Page(title='Using MongoEngine with embedded', \
         tags = ['one', 'two'],
         comments=[comment1, comment2])
-    #page.tags = ['mongodb', 'mongoengine']
+    page.tags = ['mongodb', 'mongoengine']
     page.save()
 
     print(Page.objects(tags='mongoengine').count())
 
+    john = User(name="John Smith")
+    john.save()
+
+    post = Page2(content="Test Page")
+    post.author = john
+    post.save()
+    """
+
+    Tamar=Employee(name="Tamar")
+    Tamar.save()
+    
+    myProfile=ProfilePage(content="Tamar profile page content")
+    myProfile.employee=Tamar
+    myProfile.save()
+    
+    Tamar.delete()
+    
+    
     # read yaml configuration file
     # params=read_configuration_file()
     # print(params)
